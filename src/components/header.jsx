32c743035b23f360c1bfeb72294765e4f1a92a66
Ailen <ailenmunozz@gmail.com>
2024-04-21 13:55:16 -0300

    fix mobile

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,7 +8,8 @@ export const Header = (props) => {
           <video autoPlay muted loop>
             <source src="img\intro-bg.mp4" type="video/mp4" />
           </video>
-          <img src="img/intro-bg.jpg" alt="bordados aime" className="bg-media-mobile" />
+          <img src="img/intro-bg-mobile.jpg" alt="bordados aime mobile" className="img-mobile" />
+          <img src="img/intro-bg-tablet.jpg" alt="bordados aime tablet" className="img-tablet" />
           <div className="container">
             <div className="row">
               <div className="col-md-8 col-md-offset-4 intro-text">
